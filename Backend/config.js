6f15ce103decd60e548136e835764b8faf77a4ee
thyagaalwis <thyagaalwis@gmail.com>
2025-03-16 11:22:48 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 8076;
 
-export const mongoDBURL='mongodb+srv://hasinduniran29:hasi21@cluster0.ynv5hxu.mongodb.net/Saloon_db?retryWrites=true&w=majority'
+export const mongoDBURL='mongodb+srv://chaminduathukorala:AA20020607@studentmanagement.i6j3dq6.mongodb.net/Saloon_db?retryWrites=true&w=majority'
